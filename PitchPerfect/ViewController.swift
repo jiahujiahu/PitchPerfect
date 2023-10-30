575f51b7a4bb208894aa110a79b3e194b3fc7035
Jia Hu <jia.hu0918@gmail.com>
2023-10-30 18:20:07 -0400

    viewController

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+
+    // viewDidLoad is called when your UIViewController is first loaded into memory, even before it's shown on screen.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
 
+    // ViewWillAppear however is only called when the View Controller is about to be presented on screen, but before it is actually on the screen.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+    }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        // print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+
+    @IBAction func stopRecording(_ sender: Any) {
+        // print("stop recording button was pressed")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+    }
 }
 
